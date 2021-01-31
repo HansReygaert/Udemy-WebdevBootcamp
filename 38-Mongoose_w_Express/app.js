fb38c7f360ee9463e58681163dce1dd48aa726b3
sfBlackfox <HansRDev@outlook.com>
2021-01-31 13:39:10 +0100

    Reached the edit section

--- 38-Mongoose_w_Express/app.js
+++ 38-Mongoose_w_Express/app.js
@@ -16,15 +16,18 @@ mongoose.connect(MONGOOSE_LINK
 const express =  require('express');
 const app = express(); 
 const path = require('path');
-const NODE_PORT_NUMBER = 3000;  
+const NODE_PORT_NUMBER = 3001;  
 
 app.set('views', path.join(__dirname, 'views')); 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.listen(NODE_PORT_NUMBER, ()=> {
    console.log(`Node is live on port ${NODE_PORT_NUMBER}`);
 });
-
+//Method Override----------------------------
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 // Models-------------------------------------
 const Product = require('./models/product');
@@ -33,12 +36,43 @@ const Product = require('./models/product');
 app.get('/products', async (req, res) => {
    const products = await Product.find();
    res.render('./products/index', { products });
+   console.log('rendering products/');
+});
+
+app.get('/products/new/', (req, res) => { 
+   res.render('products/new');
+   console.log('rendering products/new');
 });
 
 app.get('/products/:id', async (req,res) => {
    const { id } = req.params; 
    const product = await Product.findById(id); 
-   console.log(product); 
    res.render('products/show', { product });
-})
+   console.log('rendering products/show');
+});
 
+app.get('/products/:id/edit', async (req,res) => {
+   const { id } = req.params; 
+   const product = await Product.findById(id);  
+   res.render('products/edit', { product });
+   console.log('rendering products/show/edit');
+});
+
+app.post('/products', async (req, res) => {
+   console.log(`Posting POST data to /products/: \n ${req.body}`);
+
+   const newProduct = new Product(req.body); 
+   await newProduct.save();
+   res.redirect(`/products/${newProduct._id}`);
+});
+
+app.put('/products/:id', async (res,req) => {
+   console.log('PUT the update to the database');
+   console.log(req.params);
+
+   const { id } = req.params; 
+   const product = await Product.findByIdAndUpdate(id, 
+      req.body, {runValidators: true, new: true});
+
+   res.redirect(`/products/${product._id}`);
+});
