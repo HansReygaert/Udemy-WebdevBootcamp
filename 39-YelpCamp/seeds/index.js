2454dfe2c219a3815f16d464803efbbed190e26b
sfBlackfox <HansRDev@outlook.com>
2021-02-08 08:00:00 +0100

    Setting up seeds to add images

--- 39-YelpCamp/seeds/index.js
+++ 39-YelpCamp/seeds/index.js
@@ -33,7 +33,9 @@ const seedDB = async() => {
       const random1000 = Math.floor(Math.random() * 1000);
       const camp = new Campground({
          location: `${cities[random1000].city} , ${cities[random1000].state}`,
-         title: `${sample(descriptors)} ${sample(places)}`
+         title: `${sample(descriptors)} ${sample(places)}`,
+         description: '',
+         image: 'https://source.unsplash.com/collection/483251';
 
       });
       await camp.save();
