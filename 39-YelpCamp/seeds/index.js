007f90dbe0c699547880f6b1a54adf23dd23daae
sfBlackfox <HansRDev@outlook.com>
2021-02-08 12:30:20 +0100

    Did a small design overhaul, included bootstrap It is now visible on all pages, so that's something CSS was slightly edited so that the campgrounds page did not look so cluthered.
    babysteps here but getting there.

--- 39-YelpCamp/seeds/index.js
+++ 39-YelpCamp/seeds/index.js
@@ -31,12 +31,13 @@ const seedDB = async() => {
    await Campground.deleteMany({});
    for(let i = 0; i < 100; i++){
       const random1000 = Math.floor(Math.random() * 1000);
+      const price = Math.floor(Math.random() * 20) + 10; 
       const camp = new Campground({
          location: `${cities[random1000].city} , ${cities[random1000].state}`,
          title: `${sample(descriptors)} ${sample(places)}`,
-         description: '',
-         image: 'https://source.unsplash.com/collection/429524'
-
+         description: `A ${sample(descriptors)} campsite at ${cities[random1000].state}`,
+         image: 'https://source.unsplash.com/collection/4651015',
+         price
       });
       await camp.save();
    }
